d598f11869e8c55690e2daf7082ee1977e7bdf99
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-26 13:14:48 -0600

    Updates trails table

--- db/migrations/20180525093759_create-trails-table.js
+++ db/migrations/20180525093759_create-trails-table.js
@@ -3,7 +3,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('trails', function(t) {
     t.increments();
     t.integer('osm_id');
-    t.integer('name');
+    t.string('name');
+    t.string('state');
+    t.string('county');
     t.timestamps();
   });
 };
