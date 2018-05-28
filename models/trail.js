3b5b9470529e0112a769d9d34017c0f862d24046
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-28 14:40:11 -0600

    adds find to trail model

--- models/trail.js
+++ models/trail.js
@@ -6,6 +6,15 @@ const all = () => {
   return database.raw('SELECT * FROM trails');
 }
 
+const find = (id) => {
+  return database.raw(`SELECT trails.*,
+                      JSON_AGG(json_build_object('lat', nodes.latitude, 'lng', nodes.longitude))
+                      FROM trails JOIN nodes ON nodes.trail_id = trails.id
+                      WHERE trails.id = ?
+                      GROUP BY trails.id`, id)
+}
+
 module.exports = {
-  all
+  all,
+  find
 }
