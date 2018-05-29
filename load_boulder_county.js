1512d8172de7580bacc961120479cfb55ea9cc51
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-28 22:00:18 -0600

    Adds env. variables

--- load_boulder_county.js
+++ load_boulder_county.js
@@ -4,7 +4,7 @@ const database = require('knex')(configuration);
 var fetch = require('node-fetch')
 
 var baseUrl = "https://www.hikingproject.com/data/get-trails"
-var queryString = `?lat=40.094327&lon=-105.355688&maxResults=500&maxDistance=50&key=?`
+var queryString = `?lat=40.094327&lon=-105.355688&maxResults=500&maxDistance=50&key=${process.env.KEY}`
 
 fetch(baseUrl+queryString)
 .then(response => response.json())
