ee9e079e92b3b0f5dba0fac3a40267197504bd9f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-03 14:06:32 -0600

    Updates trail model to include mapped trails only

--- models/trail.js
+++ models/trail.js
@@ -3,7 +3,11 @@ const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 const all = () => {
-  return database.raw('SELECT * FROM trails');
+  return database.raw(`SELECT trails.*,
+                      JSON_AGG(json_build_object('leg_id', nodes.legacy_id, 'lat', nodes.latitude, 'lng', nodes.longitude) ORDER BY nodes.legacy_id)
+                      FROM trails
+                      JOIN nodes ON nodes.trail_id = trails.id
+                      GROUP BY trails.id`);
 }
 
 const find = (id) => {
