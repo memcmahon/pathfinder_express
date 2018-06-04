5a67fc0a7608aae634654a11fb2cfedfb1b14c02
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-04 15:16:22 -0600

    adds user to view resources

--- controllers/trails.js
+++ controllers/trails.js
@@ -7,18 +7,17 @@ const index = (req, res) => {
   if (req.query.min_length === undefined && req.query.max_elevation_gain === undefined) {
     Trail.all()
     .then(data => {
-      res.render('index', {trails: data.rows})
+      res.render('index', {trails: data.rows, user: req.session.user})
     })
   } else if (req.query.min_length === "" && req.query.max_length === "" && req.query.max_elevation_gain === "") {
     Trail.all()
     .then(data => {
-      res.render('index', {trails: data.rows})
+      res.render('index', {trails: data.rows, user: req.session.user})
     })
   } else {
     Trail.findByQuery(req.query.min_length, req.query.max_length, req.query.max_elevation_gain)
     .then(data => {
-      console.log(data.rows)
-      res.render('index', {trails: data.rows})
+      res.render('index', {trails: data.rows, user: req.session.user})
     })
   }
 }
@@ -26,7 +25,7 @@ const index = (req, res) => {
 const show = (req, res) => {
   Trail.find(req.params["id"])
   .then(data => {
-    res.render('trails/show', {trail: data.rows[0], nodes: data.rows[0].json_agg});
+    res.render('trails/show', {trail: data.rows[0], nodes: data.rows[0].json_agg, user: req.session.user});
   })
 }
 
