82a6f51035752b4c61ddee778fec8206fc851d86
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-05 12:43:15 -0600

    Updates column  name for trail_id

--- db/migrations/20180605100207_create-saved-trails-table.js
+++ db/migrations/20180605100207_create-saved-trails-table.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('saved_trails', function(t) {
     t.increments();
     t.integer('user_id').references('users.id');
-    t.integer('trails_id').references('trails.id');
+    t.integer('trail_id').references('trails.id');
     t.timestamps();
   });
 };
