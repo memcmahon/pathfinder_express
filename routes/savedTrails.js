6afa2510beecea737c69932bfab3959b6c5ea7e8
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-05 12:43:46 -0600

    Adds route to post/create saved_trails

--- routes/savedTrails.js
+++ routes/savedTrails.js
@@ -8,4 +8,8 @@ router.get('/', function(req, res, next) {
   savedTrailsController.index(req, res);
 });
 
+router.post('/', function(req, res, next) {
+  savedTrailsController.create(req, res)
+});
+
 module.exports = router;
