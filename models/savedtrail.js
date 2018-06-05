8592b6b86159a1ead61cd8662e3d946fb1154591
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-05 14:29:10 -0600

    Updates find by User function of the saved trail model

--- models/savedtrail.js
+++ models/savedtrail.js
@@ -3,7 +3,8 @@ const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 const findByUser = (user_id) => {
-  return database.raw(`SELECT * FROM saved_trails
+  return database.raw(`SELECT trails.*, saved_trails.user_id FROM saved_trails
+                      JOIN trails ON saved_trails.trail_id = trails.id
                       WHERE saved_trails.user_id = ?`, user_id);
 }
 
@@ -11,6 +12,9 @@ const create = (trail_id, user_id) => {
   return database.raw(`INSERT INTO saved_trails
                       (trail_id, user_id, created_at, updated_at)
                       VALUES (?, ?, ?, ?)`, [trail_id, user_id, new Date, new Date])
+          .catch((error) => {
+            console.log(error);
+          })
 }
 
 module.exports = {
