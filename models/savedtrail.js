3362461f5f62d506a96e9ddc298744026edda63d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-05 12:44:19 -0600

    Adds create function to savedtrails model

--- models/savedtrail.js
+++ models/savedtrail.js
@@ -7,6 +7,13 @@ const findByUser = (user_id) => {
                       WHERE saved_trails.user_id = ?`, user_id);
 }
 
+const create = (trail_id, user_id) => {
+  return database.raw(`INSERT INTO saved_trails
+                      (trail_id, user_id, created_at, updated_at)
+                      VALUES (?, ?, ?, ?)`, [trail_id, user_id, new Date, new Date])
+}
+
 module.exports = {
-  findByUser
+  findByUser,
+  create
 }
